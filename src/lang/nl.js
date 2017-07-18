99a684023f2d4d1641364a3b55222d6031975689
Richard van Zon <richard@relectus.com>
2017-07-18 09:31:43 +0200

    initial version

---
+++ src/lang/nl.js
@@ -0,0 +1,46 @@
+module.exports = {
+  accepted: ':attribute moet geaccepteerd worden.',
+  after: ':attribute moet een datum zijn na :after.',
+  after_or_equal: ':attribute moet een datum zijn gelijk of na :after_or_equal.',
+  alpha: ':attribute mag alleen letters bevatten.',
+  alpha_dash: ':attribute mag alleen letters, nummers, and strepen bevatten.',
+  alpha_num: ':attribute mag alleen letters en nummers bevatten.',
+  before: ':attribute moet een datum zijn eerder dan :date.',
+  before_or_equal: ':attribute moet een datum zijn gelijk of eerder :after_or_equal.',
+  between: ':attribute moet tussen :min en :max liggen',
+  confirmed: ':attribute moet bevestigd worden.',
+  email: ':attribute is geen geldig e-mailadres.',
+  date: ':attribute is geen geldige datum.',
+  def: 'Het :attribute-veld heeft fouten.',
+  digits: ':attribute moet :digits cijfers zijn.',
+  different: ':attribute en :other dienen verschillend te zijn.',
+  'in': 'Het geselecteerde :attribute is ongeldig.',
+  integer: ':attribute moet numeriek zijn.',
+  min: {
+    numeric: ':attribute dient minimaal :min te zijn.',
+    string: ':attribute dient minimaal :min karakters te bevatten.'
+  },
+  max: {
+    numeric: ':attribute mag niet groter zijn dan :max.',
+    string: ':attribute mag niet groter zijn dan :max karakters.'
+  },
+  not_in: 'Het geselecteerde :attribute is ongeldig.',
+  numeric: 'Het :attribute dient een nummer te zijn.',
+  present: 'Het :attribute dient aanwezig te zijn.',
+  required: 'Het :attribute veld is verplicht.',
+  required_if: 'Het :attribute veld is verplicht wanneer :other is :value.',
+  required_unless: 'Het :attribute veld is verplicht, tenzij :other is in :values.',
+  required_with: 'Het :attribute veld is verplicht wanneer :values aanwezig is.',
+  required_with_all: 'Het :attribute veld is verplicht wanneer :values aanwezig is.',
+  required_without: 'Het :attribute veld is verplicht wanneer :values niet aanwezig is.',
+  required_without_all: 'Het :attribute veld is verplicht wanneer geen van :values aanwezig is.',
+  same: 'Het :attribute en :other moeten hetzelfde zijn.',
+  size: {
+    numeric: ':attribute moet :size zijn.',
+    string: ':attribute moet :size karakters lang zijn.'
+  },
+  string: ':attribute moet een tekenreeks zijn.',
+  url: ':attribute is een ongeldig webadres.',
+  regex: ':attribute is ongeldig.',
+  attributes: {}
+};
